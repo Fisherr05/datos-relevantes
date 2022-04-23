31eca8f630ee93bccebc20d736118272d431a5f3
Carlos Alberto Fiallos Bejarano <carlosfiallos@hotmail.es>
2022-04-23 11:49:03 -0500

    Added Cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import estacionesRoutes from './routes/estaciones.routes.js';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(estacionesRoutes);
 export default app;
